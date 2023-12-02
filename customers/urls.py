f22b4e9e61f5b759fc239a5a56e6347ff93fa56a
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2023-12-02 17:10:23 -0300

    Create form by using in template, forms of django forms

--- customers/urls.py
+++ customers/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CustomerListView
+from .views import CustomerListView, CustomerCreateView
 
 app_name = "customers"
 urlpatterns = [
-    path('list/', CustomerListView.as_view(), name='customer-list' )
+    path('list/', CustomerListView.as_view(), name='customer-list' ),
+    path('customer_create', CustomerCreateView.as_view(), name='customer-create')
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from .models import Customer
-
+from .forms import CustomerForm
 
 class CustomerListView(ListView):
     template_name = "customers/customer_list.html"
     model = Customer
     queryset = Customer.objects.all()
 
+class CustomerCreateView(CreateView):
+    template_name = "customers/customer.html"
+    form_class = CustomerForm
+
+    def form_valid(self, form):
+        return super().form_valid(form)
