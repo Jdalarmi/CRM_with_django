057dbadde36c755abc68c7feeb3007ae6919c65c
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2023-12-02 17:56:17 -0300

    create method post for forms customer, and edit temaple customer for visuals great

--- customers/forms.py
+++ customers/forms.py
@@ -1,16 +1,30 @@
 from django import forms
 from.models import Customer
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit
 
+class DateInput(forms.DateInput):
+    input_type = "date"
 
 class CustomerForm(forms.ModelForm):
-    first_name = forms.CharField()
-    last_name = forms.CharField()
+    first_name = forms.CharField(label='Nome')
+    last_name = forms.CharField(label="Sobrenome")
     email = forms.EmailField()
-    birth_date = forms.DateField()
-    area_code = forms.CharField()
-    phone_number = forms.CharField()
-    state = forms.CharField()
-    city = forms.CharField()
+    birth_date = forms.DateField(label="Data de Nascimento", widget=DateInput())
+    area_code = forms.RegexField(
+        label="DDD",
+        regex=r"^/+?1?[0-9]{2}",
+        error_messages={"invalid": "Numero de DDD inválido"}
+        )
+    phone_number = forms.RegexField(
+        label="Numero de telefone",
+        regex=r"^/+?1?[0-9]{9, 15}",
+        error_messages={"invalid": "Numero de telefone inválido"},
+        )
+        
+    country = forms.CharField(label="Pais")
+    state = forms.CharField(label="Estado")
+    city = forms.CharField(label="Cidade")
 
 
     class Meta:
--- customers/views.py
+++ customers/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.generic import ListView, CreateView
 from .models import Customer
 from .forms import CustomerForm
+from django.urls import reverse
+
 
 class CustomerListView(ListView):
     template_name = "customers/customer_list.html"
@@ -14,3 +16,5 @@ class CustomerCreateView(CreateView):
 
     def form_valid(self, form):
         return super().form_valid(form)
+    def get_success_url(self):
+        return reverse('customers:customer-list')
